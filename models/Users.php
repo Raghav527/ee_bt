caf719987e8901019ca092eabd169be86c242b65
TECHMAHINDRA\RU00514763 <Rishi@143>
2020-01-15 22:49:08 +0530

    BT Project commit

---
+++ models/Users.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $user_id
+ * @property string $user_name
+ * @property int $user_type
+ * @property string $password
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_name', 'user_type', 'password'], 'required'],
+            [['user_type'], 'integer'],
+            [['user_name', 'password'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'user_id' => 'User ID',
+            'user_name' => 'User Name',
+            'user_type' => 'User Type',
+            'password' => 'Password',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return UsersQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new UsersQuery(get_called_class());
+    }
+}
